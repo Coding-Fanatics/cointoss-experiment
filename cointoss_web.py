f238491af6e76845e5b78b64e4dfacd067b1aae4
Ansah Mohammad <85027826+AnsahMohammad@users.noreply.github.com>
2022-01-11 15:04:53 +0530

    Added Table

--- cointoss_web.py
+++ cointoss_web.py
@@ -4,17 +4,18 @@ import numpy as np
 import streamlit as st
 from PIL import Image
 from math import factorial
+import pandas as pd
 
 #creating a variable for number of coins
 global coins
 global trial
 
 #web app UI 
-st.header(" Coin Toss experiment")
+st.header("Coin Toss Experiment")
 
 #getting the input from the user
-coins = st.slider("number of coins you're using",1,100,12)
-trial = st.slider("number of trials:",1,10000,400)
+coins = st.slider(" number of coins you're using",1,100,12)
+trial = st.slider(" number of trials:",1,10000,400)
 
 
 # to store random values
@@ -57,12 +58,17 @@ def start():
   theorotical(trial,coins)  
 
 button = st.button("show graph")
+theoretical = st.checkbox("Show Theoretical distribution Also")
+
 
 start()
 
 l = list(counts.items())
 l.sort()
 counts = dict(l)  
+
+
+
 
 print(counts)
 
@@ -72,18 +78,46 @@ if button:
   y = counts.values()
 
   plt.xlabel("values")
-  plt.ylabel("freaquency")
+  plt.ylabel("Freaquency")
+  if theoretical == 1 :
 
-  #graph variables defining
-  x_thear = [i for i in range(coins)]
-  y_thear = theor_freq
+      #graph variables defining
+      x_thear = [i for i in range(coins)]
+      y_thear = theor_freq
 
-  plt.plot(x,y)
-  plt.plot(x_thear,y_thear)
+      plt.plot(x,y)
+      plt.plot(x_thear,y_thear)
+      plt.legend(['Generated distribution','Theoretical distribution'], loc = 'upper left')
 
-  plt.legend(['Generated Random distribution','Theoretical Random distribution'], loc = 'upper right')
+      #creating table:
+      # initialize data of lists.
+      data_th = {"Theoretical Number of Heads":x_thear,"Theoretical freaquency": y_thear}
+      df_th = pd.DataFrame(data_th)
+      st.write(" ## Theoretical Random Distribution")
+      st.dataframe(df_th)
+      data = {"Number of Heads":counts.keys(),"freaquency": counts.values()}
+      df = pd.DataFrame(data)
+      st.write("## Generated Random Distribution ")
+      st.dataframe(df)
+  
+  else:
+    plt.plot(x,y)
+    #creating table:
+    # initialize data of lists.
+    data = {"Number of Heads":counts.keys(),"freaquency": counts.values()}
+    df = pd.DataFrame(data)
+    st.write("## Generated Random Distribution ")
+    st.dataframe(df)
+          
+
+
   #saving graph as a figure
   
   plt.savefig("output.jpg")
   image = Image.open('output.jpg')
+
+  st.write(" ## Random Distribution Graph")
   st.image(image, caption = 'freaquency distribution', use_column_width = True)
+
+  end_note = "\n"*10 + "Made with ❤️ \n ~'SVIT-CODING-FANATICS'"
+  st.write(end_note)
