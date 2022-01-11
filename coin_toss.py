0992c05f1ee73d627b2a07f55e27dda29d6562bc
Ansah Mohammad <85027826+AnsahMohammad@users.noreply.github.com>
2022-01-11 15:08:30 +0530

    Added Dataframe

--- coin_toss.py
+++ coin_toss.py
@@ -1,6 +1,8 @@
 from random import randint as rand
 import matplotlib.pyplot as plt
 import numpy as np
+from math import factorial
+import pandas as pd
 
 
 
@@ -40,6 +42,15 @@ def counter():
     val = []        
 
 
+theor_freq =[]
+
+def theorotical(N,n):
+  #hello
+  for r in range(n):
+    theor_freq.append( (N* factorial(n))  / ( (factorial(n-r) * factorial(r) ) * (2**n) ))
+
+  
+
 def start():
   global trial
 
@@ -48,20 +59,49 @@ def start():
     tosser()
     counter()
 
+  theorotical(trial,coins)  
+
   
+
 start()
 
 l = list(counts.items())
 l.sort()
 counts = dict(l)  
 
+data = {"Number of Heads":counts.keys(),
+"freaquency": counts.values()}
+
+df = pd.DataFrame(data)
+
+print(df)
+
+
 #plotting graph
 x = counts.keys()
 y = counts.values()
+
+#graph variables defining
+x_thear = [i for i in range(coins)]
+y_thear = theor_freq
+
+#print(x_thear,y_thear)
+
+k = np.array([str(x),str(y)])
+#print(k)
+
+data_th = {"Theoretical Number of Heads":x_thear,"Theoretical freaquency": y_thear}
+df_th = pd.DataFrame(data_th)
+print("Theoretical Random Distribution")
+print(df_th)
 
 plt.xlabel("values")
 plt.ylabel("freaquency")
 
 plt.plot(x,y)
+
+plt.plot(x_thear,y_thear)
+
+plt.legend(['Generated Random distribution','Theoretical Random distribution'], loc = 'lower right')
 
 plt.show()
